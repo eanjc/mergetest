216bdf39d590fedbdbd5d9f19718417f5fd332f6
eanjc <xqsx45zy@126.com>
2022-06-10 23:46:04 +0800

    Init...

---
+++ file1.py
@@ -0,0 +1,10 @@
+class File1Demo1:
+    def function1_A(self, input1):
+        print("function 1_A" + input1)
+
+    def function1_B(self, input1):
+        print("function 1_B" + input1)
+
+    def function1_C(self, input1):
+        print("function 1_C" + input1)
+
---
+++ file2.py
@@ -0,0 +1,11 @@
+class File2Demo1:
+    def function2_A(self, input1):
+        print("function 2_A" + input1)
+
+    def function2_B(self, input1):
+        print("function 2_B" + input1)
+
+    def function2_C(self, input1):
+        print("function 2_C" + input1)
+
+
